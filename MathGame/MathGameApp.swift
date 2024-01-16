3af7b5f88d97f6ffd0c69580b3514638c33277e1
AntonieSander <antonies9@hotmail.com>
2024-01-16 18:16:18 +0200

    Initial Commit

---
+++ MathGame/MathGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  MathGameApp.swift
+//  MathGame
+//
+//  Created by Antonie Sander on 2024/01/16.
+//
+
+import SwiftUI
+
+@main
+struct MathGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
