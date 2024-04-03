66a55e511fa0026047e8815fb4f4a7f3fa96bfc3
TheRealAnt <antonies9@hotmail.com>
2024-04-03 07:54:45 +0200

    [Bugfix] Different problem types
    - Updated UI
    - Button shadows fixed for dark mode

--- MathGame/ContentView.swift
+++ MathGame/ContentView.swift
@@ -7,6 +7,7 @@
 
 import SwiftUI
 import AVFoundation
+import Combine
 
 struct ContentView: View {
     private enum Difficulty: Int, CaseIterable, Identifiable {
@@ -15,37 +16,101 @@ struct ContentView: View {
         case hard = 999
         var id: Self { self }
     }
+    
+    private enum Problem: String, CaseIterable, Identifiable {
+        case addition = "+"
+        case subtraction = "−"
+        case division = "÷"
+        case multiplication = "×"
+        var id: Self { self }
+    }
+    
+    private enum Sound: SystemSoundID {
+        case correctAnswer = 1026
+        case buttonTap = 1104
+    }
+    
     @State private var correctAnswer = 0
     @State private var choiceArray : [Int] = [0, 1, 2, 3]
     @State private var firstNumber = 0
     @State private var secondNumber = 0
     @State private var score = 0
     @State private var selectedDifficulty: Difficulty = .easy
+    @State private var selectedProblem: Problem = .addition
+    @State private var timeRemaining = 30
+    
+//    @Environment(\.scenePhase) var scenePhase
+//    @State private var isActive = true
+//    let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
+    @Environment(\.colorScheme) var colorScheme
     
     var body: some View {
-        VStack {
-            Spacer()
-            configurePicker()
-            Spacer()
-            configureMathProblemTitle()
-            configureAnswerButtonsFirstRow()
-            configureAnswerButtonsSecondRow()
-                .padding(.bottom)
-            configureScoreTitle()
-            Spacer()
+        NavigationStack {
+            VStack {
+                List {
+                    configureDifficultyPicker()
+                    configureProblemPicker()
+                }
+                .scrollContentBackground(.hidden)
+                // configureTimer()
+                configureMathProblemTitle(for: selectedProblem)
+                configureAnswerButtonsFirstRow()
+                configureAnswerButtonsSecondRow()
+                    .padding(.bottom)
+                configureScoreTitle()
+            }
+            .onAppear(perform: {
+                generateAnswers(for: selectedProblem)
+            })
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .background(colorScheme == .light ? LinearGradient(gradient: Gradient(colors: [Color.indigo, Color.cyan]), startPoint: .top, endPoint: .bottom) : LinearGradient(gradient: Gradient(colors: [Color.indigo.opacity(0.4), Color.black]), startPoint: .topLeading, endPoint: .bottomTrailing))
+            .navigationTitle("Math Games")
+            .navigationBarTitleDisplayMode(.inline)
+//            .toolbar {
+//                Button(action: {
+//                    print("settings tapped!")
+//                }, label: {
+//                    Image(systemName: "gear")
+//                        .renderingMode(.template)
+//                        .foregroundColor(.white)
+//                })
+//                .padding()
+//            }
         }
-        .onAppear(perform: generateAnswers)
-        .frame(maxWidth: .infinity, maxHeight: .infinity)
-        .background(Color(.systemBackground))
+        // TODO: Implement timer
+        //        .onReceive(timer) { time in)
+        //            guard isActive else { return }
+        //            if timeRemaining > 0 {
+        //                timeRemaining -= 1
+        //            }
+        //        }
+        //        .onChange(of: scenePhase) {
+        //            if scenePhase == .active {
+        //                isActive = true
+        //            } else {
+        //                isActive = false
+        //            }
+        //        }
     }
     
-    private func configurePicker() -> some View {
+    private func configureTimer() -> some View {
+        AnswerButton(text: "Time: ",
+                     number: timeRemaining,
+                     width: 130,
+                     height: 45,
+                     font: .system(size: 25, weight: .bold, design: .rounded))
+        .frame(alignment: .trailing)
+        .frame(maxWidth: .infinity, alignment: .trailing)
+        .padding()
+    }
+    
+    private func configureDifficultyPicker() -> some View {
         Picker(String(describing: Difficulty.self), selection: $selectedDifficulty) {
             ForEach(Difficulty.allCases) { difficulty in
                 Text(String(describing: difficulty.id))
             }
         }
-        .pickerStyle(.segmented)
+        .pickerStyle(.automatic)
         .onChange(of: selectedDifficulty) { oldDifficulty, newDifficulty in
             switch newDifficulty {
             case .easy:
@@ -55,12 +120,34 @@ struct ContentView: View {
             case .hard:
                 selectedDifficulty = .hard
             }
-            generateAnswers()
+            generateAnswers(for: selectedProblem)
         }
     }
     
-    private func configureMathProblemTitle() -> Text {
-        Text("\(firstNumber) + \(secondNumber)")
+    private func configureProblemPicker() -> some View {
+        Picker(String(describing: Problem.self), selection: $selectedProblem) {
+            ForEach(Problem.allCases) { difficulty in
+                Text(String(describing: difficulty.id))
+            }
+        }
+        .pickerStyle(.automatic)
+        .onChange(of: selectedProblem) { oldProblem, newProblem in
+            switch newProblem {
+            case .addition:
+                selectedProblem = .addition
+            case .subtraction:
+                selectedProblem = .subtraction
+            case .division:
+                selectedProblem = .division
+            case .multiplication:
+                selectedProblem = .multiplication
+            }
+            generateAnswers(for: selectedProblem)
+        }
+    }
+    
+    private func configureMathProblemTitle(for problem: Problem) -> Text {
+        Text("\(firstNumber) \(problem.rawValue) \(secondNumber)")
             .font(.largeTitle)
             .bold()
     }
@@ -86,44 +173,52 @@ struct ContentView: View {
             let impactMed = UIImpactFeedbackGenerator(style: .medium)
             impactMed.impactOccurred()
             answerIsCorrect(answer: choiceArray[index])
-            generateAnswers()
+            generateAnswers(for: selectedProblem)
         }, label: {
             AnswerButton(number: (choiceArray[index]))
         }).buttonStyle(ButtonScaleEffect())
     }
     
     private func configureScoreTitle() -> some View {
-        Text("Score: \(score)")
-            .font(.title2)
-            .bold()
-            .foregroundStyle(Color(.label))
+        AnswerButton(text: "Score: ",
+                     number: score,
+                     width: 130,
+                     height: 45,
+                     font: .system(size: 25, weight: .bold, design: .rounded))
     }
     
     private func answerIsCorrect(answer: Int) {
         if answer == correctAnswer {
             self.score += 1
-            AudioServicesPlaySystemSound(1026)
+            AudioServicesPlaySystemSound(Sound.correctAnswer.rawValue)
         } else if score > 0 {
-            AudioServicesPlaySystemSound(1104)
+            AudioServicesPlaySystemSound(Sound.buttonTap.rawValue)
             self.score -= 1
         }
     }
     
-    private func generateAnswers() {
+    private func generateAnswers(for problem: Problem) {
         firstNumber = Int.random(in: 0...(selectedDifficulty.rawValue/2))
         secondNumber = Int.random(in: 0...(selectedDifficulty.rawValue/2))
         var answerList = [Int]()
         
-        correctAnswer = firstNumber + secondNumber
+        switch problem {
+        case .addition:
+            correctAnswer = firstNumber + secondNumber
+        case .subtraction:
+            correctAnswer = firstNumber - secondNumber
+        case .division:
+            correctAnswer = firstNumber / secondNumber
+        case .multiplication:
+            correctAnswer = firstNumber * secondNumber
+        }
         
         for _ in 0...2 {
             answerList.append(Int.random(in: 0...selectedDifficulty.rawValue))
         }
         
         answerList.append(correctAnswer)
-        
         choiceArray = answerList.shuffled()
-        
     }
 }
 
--- MathGame/Presentation/AnswerButton.swift
+++ MathGame/Presentation/AnswerButton.swift
@@ -8,41 +8,54 @@
 import SwiftUI
 
 struct AnswerButton: View {
-    var number : Int
+    var text: String = ""
+    var number: Int
+    var width: Double = 110
+    var height: Double = 110
+    var font: Font = .system(size: 35, weight: .bold, design: .rounded)
+    var buttonColor: Color = .cyan
+    @Environment(\.colorScheme) var colorScheme
+    
+    var colorSchemeLightGradient: LinearGradient = LinearGradient(gradient: Gradient(colors: [Color.indigo, Color.cyan]), startPoint: .topLeading, endPoint: .bottomTrailing)
+    
+    var colorSchemeDarkGradient: LinearGradient = LinearGradient(gradient: Gradient(colors: [Color.indigo, Color.black.opacity(0.2)]), startPoint: .topLeading, endPoint: .bottomTrailing)
     
     var body: some View {
         VStack {
-            Text("\(number)")
-                .font(.system(size: 35, weight: .bold, design: .rounded))
-                .frame(width: 110, height: 110)
+            Text("\(text)\(number)")
+                .font(font)
+                .frame(width: width, height: height)
                 .background(
                     ZStack {
-                        Color.indigo
+                        //buttonColor
+                        colorScheme == .light ? buttonColor : .indigo.opacity(0.3)
                         
-                        RoundedRectangle(cornerRadius: 110/2, style: .continuous)
-                            .foregroundStyle(.cyan)
+                        RoundedRectangle(cornerRadius: height/2, style: .continuous)
+                            .foregroundStyle(colorScheme == .light ? buttonColor : .indigo.opacity(0.3))
                             .blur(radius: 4)
                             .offset(x: -8, y: -8)
                         
-                        RoundedRectangle(cornerRadius: 110/2, style: .continuous)
+                        RoundedRectangle(cornerRadius: height/2, style: .continuous)
                             .fill(
-                                LinearGradient(gradient: Gradient(colors: [Color.indigo, Color.cyan]), startPoint: .topLeading, endPoint: .bottomTrailing)
+                                colorScheme == .light ? colorSchemeLightGradient : colorSchemeDarkGradient
                             )
                             .padding(2)
                             .blur(radius: 2)
                     }
                 )
                 .foregroundColor(Color(.systemBackground))
-                .clipShape(RoundedRectangle(cornerRadius: 110/2, style: .continuous))
+                .clipShape(RoundedRectangle(cornerRadius: height/2, style: .continuous))
                 .padding()
-                .shadow(color: Color(.secondaryLabel), radius: 15, x: 10, y: 10)
-                .shadow(color: Color(.systemBackground), radius: 15, x: -10, y: -10)
+                .shadow(color: Color(.black).opacity(0.2), radius: 15, x: 10, y: 10)
         }
     }
 }
 
 struct AnswerButton_Previews: PreviewProvider {
     static var previews: some View {
+        @Environment(\.colorScheme) var colorScheme
         AnswerButton(number: 100)
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .background(colorScheme == .light ? LinearGradient(gradient: Gradient(colors: [Color.indigo, Color.cyan]), startPoint: .top, endPoint: .bottom) : LinearGradient(gradient: Gradient(colors: [Color.indigo.opacity(0.4), Color.black]), startPoint: .top, endPoint: .bottom))
     }
 }
